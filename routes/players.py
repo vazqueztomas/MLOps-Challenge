4f1d80c4bd85a90b1bb089295bd1ae00e1bee2b1
Tomas Vazquez <vazquezt2018@gmail.com>
2023-08-31 19:42:41 -0300

    added put and post http request

--- routes/players.py
+++ routes/players.py
@@ -1,24 +1,59 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
 router = APIRouter(prefix='/players')
 
 class FootballPlayer(BaseModel):
-	id: int
+	player_id: int
 	fname: str
 	lname: str
 	age: int
 	isPlay: bool
 
-players = {
-   0: FootballPlayer(id=1,fname="Juan Roman", lname="Riquelme", age=42, isPlay=False), 
-   1: FootballPlayer(id=2,fname="Martin", lname="Palermo", age=40, isPlay=False), 
-   2: FootballPlayer(id=3,fname="Guillermo", lname="Barros Schelotto", age=40, isPlay=False), 
-   3: FootballPlayer(id=4,fname="Sebastián", lname="Battaglia", age=41, isPlay=False), 
-   4: FootballPlayer(id=5,fname="Exequiel", lname="Zeballos", age=20, isPlay=True)
-}
+players = [
+   FootballPlayer(player_id=1,fname="Juan Roman", lname="Riquelme", age=42, isPlay=False), 
+   FootballPlayer(player_id=2,fname="Martin", lname="Palermo", age=40, isPlay=False), 
+   FootballPlayer(player_id=3,fname="Guillermo", lname="Barros Schelotto", age=40, isPlay=False), 
+   FootballPlayer(player_id=4,fname="Sebastián", lname="Battaglia", age=41, isPlay=False), 
+   FootballPlayer(player_id=5,fname="Exequiel", lname="Zeballos", age=20, isPlay=True)
+]
 
 
 @router.get("/")
 async def get_players():
     return {"players": players}
+
+
+@router.get("/{player_id}")
+async def get_player_by_id(player_id: int):
+    return search_player(player_id)
+
+@router.post("/", response_model=FootballPlayer)
+async def charge_player(player: FootballPlayer):
+    try:
+        players.append(player)
+        return player
+    except Exception as err:
+        raise HTTPException(status_code=500, detail="Server error") from err
+    
+@router.put("/", response_model=FootballPlayer)
+async def update_player(player: FootballPlayer):
+    found = False
+
+    for index, saved_player in enumerate(players):
+        if saved_player.player_id == player.player_id:
+            players[index] = player
+            found = True
+            return player
+    
+    if not found:
+        raise HTTPException(status_code=404, detail="User not found")
+
+
+
+def search_player(player_id: int):
+    playerFound = filter(lambda p: p.player_id == player_id, players)
+    try:
+        return list(playerFound)[0]
+    except Exception as err:
+        raise HTTPException(status_code=404, detail="Player not found") from err
