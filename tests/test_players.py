08839066fdd4119ba7d8481b2389c84ea74d44a1
Tomas Vazquez <vazquezt2018@gmail.com>
2023-09-01 18:26:57 -0300

    created new test

---
+++ tests/test_players.py
@@ -0,0 +1,27 @@
+from fastapi.testclient import TestClient
+from main import app
+from routes import players
+
+def test_get_players():
+    client = TestClient(app)
+
+    response = client.get('/players')
+
+    assert response.status_code == 200
+    data = response.json()
+    
+def test_get_player_by_id():
+    client = TestClient(app)
+    player_id_one = {
+    "player_id": 1,
+    "fname": "Juan Roman",
+    "lname": "Riquelme",
+    "age": 42,
+    "isPlay": False
+    }
+    response = client.get("/players/1")
+
+    assert response.status_code == 200
+    data = response.json()
+
+    assert data == player_id_one
